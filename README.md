7401bce3cd090dc149d781442d25dd9e74241414
余志杰 <yuzj@sudoprivacy.com>
2024-09-12 15:25:10 +0800

    add autoTranscension

--- README.md
+++ README.md
@@ -5,9 +5,14 @@ etherealfarm: https://github.com/lvandeve/etherealfarm
 - Auto activate weather
 - Auto pick up fern
 - Auto refresh brassica
+- Auto transcension at 2h5m
 
 ## Test
 Currently tested only on version 0.14.1 (2024-09-09)
 
 ## How to use it
 You can run it in the Chrome console or use it with Tampermonkey.
+
+## TODO
+- config trigger for transcension
+- legacy activate weather, such as seed plant would not activate mist
--- auto.js
+++ auto.js
@@ -63,6 +63,81 @@ function autocRefreshBrassica() {
     loopRefresh();
 }
 
+function parseTime(timeStr) {
+    let totalSeconds = 0;
+    const timeParts = timeStr.match(/(\d+)([hms])/g);
+    if (timeParts) {
+        for (const part of timeParts) {
+            const match = part.match(/(\d+)([hms])/);
+            if (match) {
+                const [_, num, unit] = match;
+                switch (unit) {
+                    case 'h': totalSeconds += parseInt(num) * 3600; break;
+                    case 'm': totalSeconds += parseInt(num) * 60; break;
+                    case 's': totalSeconds += parseInt(num); break;
+                }
+            }
+        }
+    }
+    return totalSeconds;
+}
+
+function findAndClickTreeLevel() {
+    const pixelatedElements = document.getElementsByTagName('div');
+    for (let i = 0; i < pixelatedElements.length; i++) {
+        const element = pixelatedElements[i];
+        if (element.getAttribute('aria-label') && element.getAttribute('aria-label').includes('tree level')) {
+            element.click();
+            console.log('click tree level');
+            break;
+        }
+    }
+}
+
+function showTranscensionDialog() {
+    const transcensionDialog = document.querySelector('div[aria-description="Show the transcension dialog"]');
+    if (transcensionDialog) {
+        transcensionDialog.click();
+        console.log('click transcension dialog');
+    }
+}
+
+function clickTranscensionButton() {
+    const transcensionButton = document.querySelector('div[aria-label="regular run (dialog button)"]');
+    if (transcensionButton) {
+        transcensionButton.click();
+        console.log('excute transcension');
+    }
+}
+
+function executeTranscension() {
+    findAndClickTreeLevel();
+    showTranscensionDialog();
+    clickTranscensionButton();
+}
+
+function autoTranscension() {
+    let lastLogTime = 0;
+    function loopCheckTranscension() {
+        const timeElement = document.getElementsByClassName("efInfo efSeasonBgWinter")[0].childNodes[1];
+        const timeStr = timeElement ? timeElement.innerText : "0s";
+        const totalSeconds = parseTime(timeStr);
+        if (totalSeconds >= 2 * 3600 + 5 * 60) {
+            console.log("runtime > 2小时15分钟，auto transcension...");
+            executeTranscension();
+        } else {
+            const currentTime = new Date().getTime();
+            if (currentTime - lastLogTime >= 600000) {
+                console.log(`current runtime: ${timeStr}, runtime > 2h5m will autotranscension`);
+                lastLogTime = currentTime;
+            }
+        }
+        setTimeout(loopCheckTranscension, 60000);
+    }
+    loopCheckTranscension();
+}
+
 checkAndActivateWeather();
 autocClickFern();
 autocRefreshBrassica();
+autoTranscension();
